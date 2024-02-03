5c745f1210b65b516b6d583b723a506707abb238
vincent-the-gamer <1127620453@qq.com>
2024-02-03 12:48:17 +0800

    feat: optimize useIf and useIfElse.

--- packages/core/src/ifElse.ts
+++ packages/core/src/ifElse.ts
@@ -5,25 +5,31 @@ type IfElseRule = [boolean | "_", Handler]
 /**
  * Combine Multiple ifs
  * @param rules 
+ * @returns an array, the results of rules are in the same of their original index.
+ *          true statements get the return value of their inner function, while the falses gets null.
  */
 export function useIf(rules: IfRule[]) {
+    const results = []
     for (const [condition, handler] of rules) {
         if(condition){
-            handler(condition)
+            results.push(handler(condition))
+        } else {
+            results.push(null)
         }
     }
+    return results
 }
 
 /**
  * Create a if-else chain
  * @param rules 
+ * @returns result of true statement.
  */
 export function useIfElse(rules: IfElseRule[]) {
     for (const [condition, handler] of rules) {
         const willCall: boolean = (typeof condition === "boolean" && condition) || condition === "_"
         if(willCall){
-            handler(condition)
-            break
+            return handler(condition)
         } else {
            continue
         }
--- packages/core/test/ifElse.test.ts
+++ packages/core/test/ifElse.test.ts
@@ -1,23 +1,28 @@
-// run `pnpm run dev test/ifElse.test.ts`
-
+import { expect, test } from "vitest"
 import { useIf, useIfElse } from "../src/ifElse"
 
-function sayHaha(){
-    console.log("haha")
-}
 
-// useIf([
-//     [2 > 1, sayHaha], // handler will be called
-//     // @ts-expect-error
-//     [2 === 1, sayHaha], // handler will not be called
-// ])
+test("useIf", () => {
+    const result = useIf([
+        [2 > 1, () => "yeah, 2 > 1"], // handler will be called
+        // @ts-expect-error
+        [2 === 1, () => "yeah, 2 === 1"], // handler will not be called
+    ])
+    expect(result).toStrictEqual(
+        ["yeah, 2 > 1", null]
+    )
+})
 
 // output: ccc
-useIfElse([
-    [2 < 1, () => {console.log("aaa")}], // continue
-    // @ts-expect-error
-    [2 === 1, () => {console.log("bbb")}], // continue
-    [2 > 1, () => {console.log("ccc")}], // true, call the func
-    ["_", () => {console.log("not reached")}] // else
-])
+test("useIfElse",() => {
+    const result = useIfElse([
+        [2 < 1, () => "aaa"], // continue
+        // @ts-expect-error
+        [2 === 1, () => "bbb"], // continue
+        [2 > 1, () => "ccc"], // true, call the func
+        ["_", () => "not reached"] // else
+    ])
+    expect(result).toBe("ccc")
+})
+
 
